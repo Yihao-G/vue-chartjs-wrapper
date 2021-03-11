1711c5cbe26579e0bce4492738a4fb95a2cde044
Yihao Gao <gao@yihao.moe>
2021-03-11 19:52:20 +1030

    Do not emit chart:destroy if the chart instance is null

--- src/functions/cleanupChartInstance.ts
+++ src/functions/cleanupChartInstance.ts
@@ -3,7 +3,9 @@ import Chart from 'chart.js'
 import { ChartEmitFn } from '@/types/emitFn'
 
 export default function cleanupChartInstance (chartInstance: Ref<Chart | null>, emit: ChartEmitFn): void {
-  chartInstance.value?.destroy()
-  chartInstance.value = null
-  emit('chart:destroy')
+  if (chartInstance.value !== null) {
+    chartInstance.value.destroy()
+    chartInstance.value = null
+    emit('chart:destroy')
+  }
 }
